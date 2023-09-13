cb438fc73076fc11ad3a3b165cbbd6b5b71de3fa
LEE SUNG AE <138199763+2frost@users.noreply.github.com>
2023-09-13 19:59:22 +0900

    like add
    like add

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import logMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+import likesRouter from './routes/likes.router.js';
 import UsersRouter from './routes/users.router.js';
 import PostsRouter from './routes/posts.router.js';
 import CommentsRouter from './routes/comments.router.js';
@@ -16,7 +17,7 @@ const PORT = 3017;
 app.use(logMiddleware); // 최상단위치
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [UsersRouter, PostsRouter, CommentsRouter]);
+app.use('/api', [likesRouter, UsersRouter, PostsRouter, CommentsRouter]);
 app.use(ErrorHandlingMiddleware); // 전역미들웨어중 에러핸들링미들웨어는 **최하단** 에 위치해야함 
 
 app.listen(PORT, () => {
